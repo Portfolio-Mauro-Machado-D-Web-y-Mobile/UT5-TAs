91d7a0a2c83a32d177e49728aff77f6da41e79ae
MauroMachado <mauro.machadoc@correo.ucu.edu.uy>
2024-10-30 18:35:01 -0300

    uwu1

---
+++ src/Home/index.jsx
@@ -0,0 +1,36 @@
+import { useState } from "react";
+import { Button, StyleSheet, Text, View, TextInput } from "react-native";
+
+const Home = () => {
+    const [text, setText] = useState("");
+
+    return (
+        <View>
+            <TextInput 
+                style={styles.textInput}
+                onChangeText={text => setText(text)}
+            />
+
+            <Text style={styles.text}>
+                {text}
+            </Text>
+        </View>
+    )
+}
+
+const styles = StyleSheet.create({
+    textInput: {
+        height: 40,
+        borderColor: 'gray',
+        borderWidth: 1,
+        marginBottom: 10,
+        paddingLeft: 5,
+    },
+    text: {
+        fontSize: 20,
+        textAlign: 'center',
+        margin: 10,
+    }
+});
+
+export default Home;
