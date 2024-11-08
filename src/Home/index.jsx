ebff4df99b85ecbaeb33ac05723c8241bff5463d
Mauro Ismael Machado Craigdallie <mauro.machadoc@correo.ucu.edu.uy>
2024-11-08 13:55:03 -0300

    uwu10

--- src/Home/index.jsx
+++ src/Home/index.jsx
@@ -1,68 +1,54 @@
-import { useState, useEffect } from "react";
-import { Button, StyleSheet, Text, View, TextInput, Image, ScrollView, Dimensions, PixelRatio, Platform } from "react-native";
-import { Swipeable } from "react-native-gesture-handler";
+import * as Location from 'expo-location';
+import { useEffect, useState } from 'react';
+
+
+
+import { StyleSheet, Platform, View, Text } from "react-native";
 import { SafeAreaView } from "react-native-safe-area-context";
 
 const Home = () => {
-    const isAndroid = Platform.OS === 'android';
+    const [location, setLocation] = useState(null);
+  const [errorMsg, setErrorMsg] = useState(null);
 
-    return (
-        <SafeAreaView style={isAndroid ? styles.containerAndroid : styles.containerApple}>
-            <Text>OS: {Platform.OS} {'\n'}{'\n'}VibeCheckTest:</Text>
-            <Text style={isAndroid ? styles.textAndroid : styles.textApple}>
-                 {isAndroid? 'Passed' : 'Failed'} {'\n'}
-            </Text>
-        </SafeAreaView>
-    );
-};
+  useEffect(() => {
+    (async () => {
+      
+      let { status } = await Location.requestForegroundPermissionsAsync();
+      if (status !== 'granted') {
+        setErrorMsg('Permission to access location was denied');
+        return;
+      }
+
+      let location = await Location.getCurrentPositionAsync({});
+      setLocation(location);
+    })();
+  }, []);
+
+  let text = 'Waiting..';
+  if (errorMsg) {
+    text = errorMsg;
+  } else if (location) {
+    text = JSON.stringify(location);
+  }
+
+  return (
+    <View style={styles.container}>
+      <Text style={styles.paragraph}>{text}</Text>
+    </View>
+  );
+}
 
 const styles = StyleSheet.create({
-    containerAndroid: {
-        flex: 1,
-        padding: 16,
-        justifyContent: 'center',
-        backgroundColor: '#f0f0f0',
-    },
-    subtitleAndroid: {
-        fontSize: 18,
-        fontWeight: 'bold',
-        marginBottom: 8,
-        color: 'red',
-    },
-    textAndroid: {
-        fontSize: 16,
-        marginBottom: 8,
-        color: 'green',
-    },
-    imageAndroid: {
-        width: 300,
-        height: 450,
-        marginBottom: 10,
-        marginTop: 10,
-    },
-    containerApple: {
-        flex: 1,
-        padding: 8,
-        justifyContent: 'center',
-        backgroundColor: '#ffffff',
-    },
-    subtitleApple: {
-        fontSize: 14,
-        fontWeight: 'bold',
-        marginBottom: 4,
-        color: '#333',
-    },
-    textApple: {
-        fontSize: 120,
-        marginBottom: 4,
-        color: 'red',
-    },
-    imageApple: {
-        width: 150,
-        height: 225,
-        marginBottom: 5,
-        marginTop: 5,
-    },
+  container: {
+    flex: 1,
+    alignItems: 'center',
+    justifyContent: 'center',
+    padding: 20,
+  },
+  paragraph: {
+    fontSize: 18,
+    textAlign: 'center',
+  },
 });
 
 export default Home;
